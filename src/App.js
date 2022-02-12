bef97f71b9f73ba79d4634211ee45cbdaaa26fee
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-02-12 23:24:03 +0530

    digital-clock created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
+import Clock from "./Clock";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Digital Watch</h1>
+      <Clock />
     </div>
   );
 }
